7ac57003ecb2e11df7de65ca26652277e3bc453d
Ozgur Guzeldereli <ozgur.guzeldereli.24@ucl.ac.uk>
2025-03-06 15:09:22 +0000

    Init

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

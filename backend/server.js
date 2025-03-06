92339fe536ec77843561379e8972d7c92b88f168
Ozgur Guzeldereli <ozgur.guzeldereli.24@ucl.ac.uk>
2025-03-06 15:30:15 +0000

    Fixed database connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" })); // Vite uses port 5173
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB Atlas."))
+    .catch((err) => console.log("An error occured during MongoDB Atlas connection."));
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");

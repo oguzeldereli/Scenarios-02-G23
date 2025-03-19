276991c9c7443b4410128a7de5a926e34beec337
Ozgur Guzeldereli <ozgur.guzeldereli.24@ucl.ac.uk>
2025-03-19 14:37:59 +0000

    Added text editor skeleton

---
+++ frontend/src/components/textEditor/TextEditor.jsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+
+import { Button, Input, TextArea } from "react-aria-components";
+
+
+export default function TextEditor()
+{
+    return (
+        <>
+        <Input />
+        <RTFArea />
+        </>
+    )
+}

3a47f6f9ae9aaaa7d46ff28b311ac73b00968475
mariha-s <msubhan0106@gmail.com>
2025-03-23 16:19:52 +0000

    added research to document types

--- backend/models/document.model.js
+++ backend/models/document.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const documentSchema = new mongoose.Schema({
     title:{type: String, required: true},
     content: {type: String, required: true},
-    type: {type: String, enum: ["scene", "location", "character"], required: true}, // specifies category
+    type: {type: String, enum: ["scene", "location", "character", "research"], required: true}, // specifies category
     project: {type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true},
 }, {
     timestamps: true //created at, updated at
